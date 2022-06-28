a9101febb2382c3507fb645a0d6bcff52bc5efac
Libiojunio <libiojss@gmail.com>
2022-06-27 21:32:36 -0300

    Solução parcial para trocar o ultimo operador.

--- src/components/Calculadora/Calculadora.jsx
+++ src/components/Calculadora/Calculadora.jsx
@@ -3,7 +3,8 @@ import './Calculadora.css';
 import {Box} from '@mui/material';
 import {ContainerComponente} from '../ContainerComponente/ContainerComponente';
 import {Botao} from '../Botao/Botao';
-const arrOperacoes = ['*', '/', '+', '.', '-'];
+
+const ARR_OPERACOES = ['*', '/', '+', '.', '-'];
 
 export class Calculadora extends React.Component {
   constructor(props) {
@@ -17,11 +18,11 @@ export class Calculadora extends React.Component {
   inputNum = (e) => {
     const state = this.state;
     let input = e.target.innerText.toLowerCase().replaceAll('x', '*');
-     if (
-       this.state.num === 0 ||
-       (arrOperacoes.includes(this.state.num[state.num.length - 1]) && arrOperacoes.includes(input))
-      ){
+    if (this.state.num === 0) {
       state.num = input;
+    }
+    else if (ARR_OPERACOES.includes(this.state.num[state.num.length - 1]) && ARR_OPERACOES.includes(input)) {
+      state.num = state.num.replace(state.num[state.num.length - 1], input);
     }
     else {
       state.num += input;
@@ -68,7 +69,7 @@ export class Calculadora extends React.Component {
     console.log('state', this.state);
     return (
       <ContainerComponente>
-        <Box m={(12)} />
+        <Box m={(12)}/>
         <h1 className="result">{this.state.num}</h1>
 
         <Botao titulo={'AC'} className={classNameOrange} disabled={disabled} onClick={this.clear}/>
@@ -76,19 +77,19 @@ export class Calculadora extends React.Component {
         <Botao titulo={'%'} className={classNameOrange} disabled={disabled} onClick={this.porcentagem}/>
         <Botao titulo={'/'} className={classNameOrange} disabled={disabled} onClick={this.inputNum}/>
 
-        <Botao titulo={'7'} className='gray' onClick={this.inputNum}/>
-        <Botao titulo={'8'} className='gray' onClick={this.inputNum}/>
-        <Botao titulo={'9'} className='gray' onClick={this.inputNum}/>
+        <Botao titulo={'7'} className="gray" onClick={this.inputNum}/>
+        <Botao titulo={'8'} className="gray" onClick={this.inputNum}/>
+        <Botao titulo={'9'} className="gray" onClick={this.inputNum}/>
         <Botao titulo={'x'} className={classNameOrange} disabled={disabled} onClick={this.inputNum}/>
 
-        <Botao titulo={'4'} className='gray' onClick={this.inputNum}/>
-        <Botao titulo={'5'} className='gray' onClick={this.inputNum}/>
-        <Botao titulo={'6'} className='gray' onClick={this.inputNum}/>
+        <Botao titulo={'4'} className="gray" onClick={this.inputNum}/>
+        <Botao titulo={'5'} className="gray" onClick={this.inputNum}/>
+        <Botao titulo={'6'} className="gray" onClick={this.inputNum}/>
         <Botao titulo={'-'} className={classNameOrange} disabled={disabled} onClick={this.inputNum}/>
 
-        <Botao titulo={'1'} className='gray' onClick={this.inputNum}/>
-        <Botao titulo={'2'} className='gray' onClick={this.inputNum}/>
-        <Botao titulo={'3'} className='gray' onClick={this.inputNum}/>
+        <Botao titulo={'1'} className="gray" onClick={this.inputNum}/>
+        <Botao titulo={'2'} className="gray" onClick={this.inputNum}/>
+        <Botao titulo={'3'} className="gray" onClick={this.inputNum}/>
         <Botao titulo={'+'} className={classNameOrange} disabled={disabled} onClick={this.inputNum}/>
 
         <Botao titulo={'0'} className="btnZero" disabled={disabled} onClick={this.inputNum}/>
@@ -97,6 +98,6 @@ export class Calculadora extends React.Component {
       </ContainerComponente>
 
     );
-}
+  }
 
 }
